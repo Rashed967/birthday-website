ea5af8684c44ce987beccc51bfdf88f8f644ab72
Rashed967 <rashedhasan967.com>
2023-07-31 18:01:34 +0600

    added some code

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Timer from '../../../Timer/Timer';
+
+const Home = () => {
+    return (
+        <div>
+            <h2>Home page</h2>
+            <Timer></Timer>
+        </div>
+    );
+};
+
+export default Home;
